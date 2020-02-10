383e8e3ece8614cd6a547dd2da583ca25938e087
Austin Rhoads <austinrrhoads@ymail.com>
2020-02-10 23:32:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,8 +57,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 lang = programmer_hash[:dennis_ritchie][:languages]
-		 return lang
+		 return programmer_hash[:dennis_ritchie][:languages]
+		 
 end
 
 def adding_matz
